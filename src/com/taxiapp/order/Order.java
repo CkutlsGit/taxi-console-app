32c0346d7f90d12d14749e70e6441278af7150a7
Kostya <ckutls228@gmail.com>
2026-02-08 11:38:21 +0300

    init Order

---
+++ src/com/taxiapp/order/Order.java
@@ -0,0 +1,11 @@
+package com.taxiapp.order;
+
+public class Order {
+    public static String callOrder() {
+        return "test";
+    }
+
+    public static int getOrderTariff() {
+        return 300;
+    }
+}
