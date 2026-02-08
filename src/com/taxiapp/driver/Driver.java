3b8c351bac29fdcc00d9ae1d33c96a42ffabb0c5
Kostya <ckutls228@gmail.com>
2026-02-08 11:46:26 +0300

    added driver menu in Driver

--- src/com/taxiapp/driver/Driver.java
+++ src/com/taxiapp/driver/Driver.java
@@ -1,7 +1,34 @@
 package com.taxiapp.driver;
 
-public class Driver {
-    public static void callDriverMenu() {
+import com.taxiapp.order.OrderDriver;
 
+import java.util.Scanner;
+
+public class Driver {
+    public static void callDriverMenu(Scanner scanner) {
+        while (true) {
+            System.out.println("Меню водителя.");
+            System.out.println("1. Посмотреть список заказов");
+            System.out.println("2. Взять заказ");
+            System.out.println("9. Вернуться в главное меню");
+            System.out.println("0. Выйти из программы");
+            System.out.println("Выбор: ");
+
+            if (scanner.hasNextInt()) {
+                int choice = scanner.nextInt();
+
+                switch (choice) {
+                    case 1 -> OrderDriver.getOrders();
+                    case 2 -> OrderDriver.takeOrder();
+                    case 9 -> { return; }
+                    case 0 -> System.exit(0);
+                    default -> System.out.println("Данной операции не существует. Введите 0, 1, 2 или 9");
+                }
+            }
+            else {
+                System.out.println("Укажите число.");
+                scanner.next();
+            }
+        }
     }
 }
