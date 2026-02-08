a2d719bec559959ef58e773c6d34c7752d0f3769
Kostya <ckutls228@gmail.com>
2026-02-08 11:37:41 +0300

    added client menu in Client

--- src/com/taxiapp/client/Client.java
+++ src/com/taxiapp/client/Client.java
@@ -1,7 +1,34 @@
 package com.taxiapp.client;
 
-public class Client {
-    public static void callClientMenu() {
+import com.taxiapp.order.Order;
 
+import java.util.Scanner;
+
+public class Client {
+    public static void callClientMenu(Scanner scanner) {
+        while (true) {
+            System.out.println("Меню клиента.");
+            System.out.println("1. Вызвать такси");
+            System.out.println("2. Узнать текущий тариф");
+            System.out.println("9. Вернуться в главное меню");
+            System.out.println("0. Выйти из программы");
+            System.out.println("Выбор: ");
+
+            if (scanner.hasNextInt()) {
+                int choice = scanner.nextInt();
+
+                switch (choice) {
+                    case 1 -> Order.callOrder();
+                    case 2 -> Order.getOrderTariff();
+                    case 9 -> { return; }
+                    case 0 -> System.exit(0);
+                    default -> System.out.println("Данной операции не существует. Введите 0, 1, 2 или 9");
+                }
+            }
+            else {
+                System.out.println("Укажите число.");
+                scanner.next();
+            }
+        }
     }
 }
